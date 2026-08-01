1ee1426fa8eb947b2085cd068a029ed4a3585452
accrevaddftah-alt <acc.revaddftah@gmail.com>
2026-08-01 18:07:00 +0700

    update

--- script.js
+++ script.js
@@ -522,7 +522,7 @@ const renderLombaOptions = (category, preselected = []) => {
 
     document.addEventListener('DOMContentLoaded', () => {
 
-    const batasWaktu = new Date("2026-07-31T07:50:00+07:00");
+    const batasWaktu = new Date("2026-08-17T07:00:00+07:00");
     const sekarang = new Date();
 
     if (sekarang >= batasWaktu) {
